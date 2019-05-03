b2cec823aef2080b20847df253673704071f3e76
Samuel <diogosamuelfernandes@outlook.com>
2019-05-03 17:59:07 +0100

    Update Agency.h

--- Agency.h
+++ Agency.h
@@ -13,19 +13,6 @@
 using namespace std;
 
 class Agency{
- private:
-  string name; // name of the  agency
-  unsigned int VATnumber; // VAT number of the agency
-  Address address; // address of the agency
-  string URL; // URL of the agency Web site
-  vector<Client> clients; // vector to store the existing clients
-  vector<Packet> packets; // vector to store the existing packets
-  
-  bool clientsInfoHasChanged; // flag that is set to "true" if at least one client has been changed/added/deleted
-  bool packetsInfoHasChanged; // flag that is set to "true" if at least one packet has been changed/added/deleted
-  unsigned int maxClientsId; // maximum value among all clients identifiers
-  unsigned int maxPacketsId; // maximum value among all packets identifiers
-
  public:
   Agency(string fileName);
 
@@ -49,5 +36,17 @@ class Agency{
   // other methods */
   
   friend ostream& operator<<(ostream& out, const Agency & agency);
+private:
+	string name; // name of the  agency
+	unsigned int VATnumber; // VAT number of the agency
+	string URL; // URL of the agency Web site
+	Address address; // address of the agency
+	vector<Client> clients; // vector to store the existing clients
+	vector<Packet> packets; // vector to store the existing packets
+
+	bool clientsInfoHasChanged; // flag that is set to "true" if at least one client has been changed/added/deleted
+	bool packetsInfoHasChanged; // flag that is set to "true" if at least one packet has been changed/added/deleted
+	unsigned int maxClientsId; // maximum value among all clients identifiers
+	unsigned int maxPacketsId; // maximum value among all packets identifiers
 
 };
