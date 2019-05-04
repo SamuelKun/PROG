d634e8511e67804cf6fa1a74f1352565a78fd3e5
Hugomguima <50011557+Hugomguima@users.noreply.github.com>
2019-05-04 10:23:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -34,6 +34,21 @@ int main()
   cout << endl;
   agency.showPackets();
  
+
+  system("CLS");
+  char menu_1;            //Menu Principal
+
+  cout << "Main Menu" << endl << endl;
+  cout << "Select the number of the correct operation  " << endl << endl;
+
+  cout << "[1] Manage Clients " << endl;
+  cout << "[2] Manage Packs " << endl;
+  cout << "[3] View Information " << endl;
+  cout << "[4] Buy Packs " << endl;
+  cout << "[0] Leave the programm " << endl << endl;
+
+  cin >> menu_1;
+
   _getwch();
   return 0;
 
