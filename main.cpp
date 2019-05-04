8d7281029ff9564df23cfaa9f6d3dfd63bcb6c16
Samuel <diogosamuelfernandes@outlook.com>
2019-05-04 10:26:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,24 @@
 
 using namespace std;
 
+void menu(Agency agency)
+{
+	system("CLS");
+	char menu_1;            //Menu Principal
+
+	cout << "Main Menu" << endl << endl;
+	cout << "Select the number of the correct operation  " << endl << endl;
+
+	cout << "[1] Manage Clients " << endl;
+	cout << "[2] Manage Packs " << endl;
+	cout << "[3] View Information " << endl;
+	cout << "[4] Buy Packs " << endl;
+	cout << "[0] Leave the programm " << endl << endl;
+
+	cin >> menu_1;
+}
+
+
 int main()
 {
 	ifstream AgencyFile;
@@ -35,20 +53,9 @@ int main()
   agency.showPackets();
  
 
-  system("CLS");
-  char menu_1;            //Menu Principal
-
-  cout << "Main Menu" << endl << endl;
-  cout << "Select the number of the correct operation  " << endl << endl;
-
-  cout << "[1] Manage Clients " << endl;
-  cout << "[2] Manage Packs " << endl;
-  cout << "[3] View Information " << endl;
-  cout << "[4] Buy Packs " << endl;
-  cout << "[0] Leave the programm " << endl << endl;
-
-  cin >> menu_1;
-
+  while (true) {
+	  menu(agency);
+  }
   _getwch();
   return 0;
 
